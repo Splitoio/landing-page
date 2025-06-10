67b0e85c5d20254b8aa22a7c8b60ee50082621fb
Cygnus <cygnusdev04@gmail.com>
2025-06-11 05:09:38 +0530

    Update Header component text and styling for clarity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,12 @@ export default function Header() {
         <img src="/logo.svg" alt="Splito" className="h-14 mt-5" />
       </div>
 
-      <div className="inline-block bg-zinc-900/50 text-sm text-gray-300 px-2 rounded-full mb-6 border border-zinc-800">
+      <div className="inline-block bg-zinc-900/50 text-sm text-gray-300 px-2 rounded-full mb-6 border border-zinc-800 uppercase">
         <span className="inline-block w-2 h-2 rounded-full font-bold bg-red-700 mr-2 animate-glow" />
-        AVAILABLE IN EARLY 2025
+        Rolling out in 2025, Stay tuned
       </div>
 
-      <h1 className="text-5xl  tracking-tight">
-        Get early access
-      </h1>
+      <h1 className="text-5xl  tracking-tight">Get early access</h1>
       <p className="text-zinc-500 text-l mt-2">
         Simplify shared expenses, settle on-chain effortlessly!
       </p>
